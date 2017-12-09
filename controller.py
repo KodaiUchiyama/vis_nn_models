28c5d7959124732186727f62a1cc5c69fdb0251b
KodaiUchiyama <32435168+KodaiUchiyama@users.noreply.github.com>
2017-12-09 14:59:58 +0900

    controller.py　AlexNet対応

--- controller.py
+++ controller.py
@@ -14,12 +14,14 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os
 from exp_models.CNN_LSTM_models import *  # Contains the models we are testing
+from exp_models.ALEX_models import *
 from model_utils import *  # Contains some useful functions for loading datasets
 from keras.models import load_model
-
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
 #########################
 #Set data_name
-train_data_name = 'vis_train_dataX_dataY.h5'  # Set to name of h5py dataset
+train_data_name = 'vis_trim_dataX_dataY.h5'  # Set to name of h5py dataset
 test_data_name = 'vis_test_dataX_dataY.h5'
 """
 Main function call for doing random hyperparameter search
@@ -38,7 +40,7 @@ def main():
 
         # random search over logarithmic space of hyperparameters
         #lr = 10**np.random.uniform(-3.0,-6.0)
-        lr = 0.1
+        lr = 0.001
         #ws = 10**np.random.uniform(-2.0,-4.0)
         ws = 0.01
         #learning_rates.append(lr)
@@ -52,7 +54,7 @@ model_utils内の関数
                 channels = 15
                 audio_vector_dim = 42 = dataY_sample.shape[0]
         """
-        frame_h, frame_w, channels, audio_vector_dim = returnH5PYDatasetDims(data_name='vis_train_dataX_dataY.h5')
+        frame_h, frame_w, channels, audio_vector_dim = returnH5PYDatasetDims(data_name = train_data_name)
 
         """Load full dataset as an HDF5 matrix object for use in Keras model
 
@@ -62,15 +64,40 @@ model_utils内の関数
                     dataX_test.shape = (4000,90,160,15)
                     dataY_test.shape = (4000,42)
         """
-        dataX_train, dataY_train = load5hpyTrainData(data_name = 'vis_train_dataX_dataY.h5')
-        dataX_test, dataY_test = load5hpyTestData(data_name = 'vis_test_dataX_dataY.h5')
+        dataX_train, dataY_train = load5hpyTrainData(data_name = train_data_name)
+        #dataX_test, dataY_test = load5hpyTestData(data_name = test_data_name)
 
+        # dataYを主成分分析して42次元から10次元に次元削減する
+        # PCA
+        # 次元数10まで圧縮
+        pca = PCA(n_components=10)
+        dataY_train_pca = pca.fit_transform(dataY_train)
+        print('dataY_train_pca shape: {}'.format(dataY_train_pca.shape))
+        # 寄与率
+        print('explained variance ratio train: {}'.format(pca.explained_variance_ratio_))
+        #データをトレーニングとテストに分割
+        #dataX_train, dataX_test, dataY_train, dataY_test = train_test_split(dataX_train, dataY_train, test_size=0.1, random_state=1)
+        #print("---decomposed and splited dataset shape---")
+        #print("dataX_train.shape:".format(dataX_train.shape))
+        #print("dataY_train.shape:".format(dataY_train.shape))
+        #print("dataX_test.shape:".format(dataX_test.shape))
+        #print("dataY_test.shape:".format(dataY_test.shape))
+        #pca_test = PCA(n_components=10)
+        #dataY_test_pca = pca_test.fit_transform(dataY_test)
+        #print('dataY_train_pca shape: {}'.format(dataY_test_pca.shape))
+        # 寄与率
+        #print('explained variance ratio of test: {}'.format(pca_test.explained_variance_ratio_))
         # create defined model with given hyper parameters
+        '''
         model = CNN_LSTM_model(image_dim=(frame_h,frame_w,channels),
                                audio_vector_dim=audio_vector_dim,
                                learning_rate=lr,
                                weight_init=ws)
-
+        '''
+        model = AlexNet_model(image_dim=(frame_h,frame_w,channels),
+                               audio_vector_dim=audio_vector_dim,
+                               learning_rate=lr,
+                               weight_init=ws)
         # load custom callbacksカスタムコールバック!?あとでサーベイ
         #loss_history = LossHistory()
         #acc_history = AccuracyHistory()
@@ -78,11 +105,11 @@ model_utils内の関数
 
         # train the model
         fit=model.fit(dataX_train,
-                  dataY_train,
+                  dataY_train_pca,
                   shuffle='batch',
-                  epochs=5,
-                  batch_size=1000,  # 10000 is the maximum number of samples that fits on TITANX 12GB Memory
-                  validation_data=(dataX_test, dataY_test),
+                  epochs=10,
+                  batch_size=20,  # 10000 is the maximum number of samples that fits on TITANX 12GB Memory
+                  #validation_data=(dataX_test, dataY_test),
                   verbose=1)
                   #callbacks = callbacks_list)
         model.save('my_model.h5')
