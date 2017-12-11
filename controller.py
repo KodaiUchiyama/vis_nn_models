8bcd47acd68a37d1b36c965bdf132b9ab008748d
KodaiUchiyama <32435168+KodaiUchiyama@users.noreply.github.com>
2017-12-11 16:05:55 +0900

    two or one stream の選択コントローラー更新

--- controller.py
+++ controller.py
@@ -1,14 +1,4 @@
 # coding:utf-8
-'''
-Lamtharn (Hanoi) Hantrakul
-29/4/2017
-
-=====================
-HYPERPARAMETER SEARCH
-=====================
-This is a controller for doing hyperparameter search over models used in the project.
-
-'''
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -20,10 +10,12 @@ from model_utils import *  # Contains some useful functions for loading datasets
 from keras.models import load_model
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
+import sys
+from keras.utils.io_utils import HDF5Matrix
 #########################
 #Set data_name
 train_data_name = 'vis_trim_dataX_dataY.h5'  # Set to name of h5py dataset
-test_data_name = 'vis_test_dataX_dataY.h5'
+test_data_name = 'vis_trim_test_dataX_dataY.h5'
 """
 Main function call for doing random hyperparameter search
 ハイパパラメータ生成、種はランダム
@@ -54,7 +46,7 @@ model_utils内の関数
             Example returns:
                 frame_h = 90
                 frame_w = 160
-                channels = 15
+                channels = 3
                 audio_vector_dim = 42 = dataY_sample.shape[0]
         """
         frame_h, frame_w, channels, audio_vector_dim = returnH5PYDatasetDims(data_name = train_data_name)
@@ -67,57 +59,73 @@ model_utils内の関数
                     dataX_test.shape = (4000,90,160,15)
                     dataY_test.shape = (4000,42)
         """
-        dataX_train, dataY_train, dataZ_train = load5hpyTrainData(data_name = train_data_name)
-        #dataX_test, dataY_test = load5hpyTestData(data_name = test_data_name)
+        dataX_train, dataY_train = load5hpyTrainData(data_name = train_data_name)
+        #dataX_test, dataY_test, dataZ_test = load5hpyTestData(data_name = test_data_name)
+        
+        #メモリ節約のためRGB画像だけはここで読み込み
+        data_dir = '/home/KODAI/MATLAB_vis_master/'
+        data_file = data_dir + train_data_name
 
+        dataZ_train = HDF5Matrix(data_file, 'dataZ_train',start=0,end=60000)
+        print("converting h5py to numpy...(only dataX Z  and dataY)")
+        dataZ_train = np.array(dataZ_train)
+
+        print("dataX_train.shape:", dataX_train.shape)
+        print("dataY_train.shape:", dataY_train.shape)
+        print("dataZ_train.shape:", dataZ_train.shape)
+        #print("dataX_test.shape:", dataX_test.shape)
+        #print("dataY_test.shape:", dataY_test.shape)
+        #print("dataZ_test.shape:", dataZ_test.shape)
         # dataYを主成分分析して42次元から10次元に次元削減する
         # PCA
         # 次元数10まで圧縮
+        
         pca = PCA(n_components=10)
         dataY_train_pca = pca.fit_transform(dataY_train)
         print('dataY_train_pca shape: {}'.format(dataY_train_pca.shape))
         # 寄与率
         print('explained variance ratio train: {}'.format(pca.explained_variance_ratio_))
-        #データをトレーニングとテストに分割
-        #dataX_train, dataX_test, dataY_train, dataY_test = train_test_split(dataX_train, dataY_train, test_size=0.1, random_state=1)
-        #print("---decomposed and splited dataset shape---")
-        #print("dataX_train.shape:".format(dataX_train.shape))
-        #print("dataY_train.shape:".format(dataY_train.shape))
-        #print("dataX_test.shape:".format(dataX_test.shape))
-        #print("dataY_test.shape:".format(dataY_test.shape))
-        #pca_test = PCA(n_components=10)
-        #dataY_test_pca = pca_test.fit_transform(dataY_test)
-        #print('dataY_train_pca shape: {}'.format(dataY_test_pca.shape))
+        '''
+        pca_test = PCA(n_components=10)
+        dataY_test_pca = pca_test.fit_transform(dataY_test)
+        print('dataY_train_pca shape: {}'.format(dataY_test_pca.shape))
         # 寄与率
-        #print('explained variance ratio of test: {}'.format(pca_test.explained_variance_ratio_))
-        # create defined model with given hyper parameters
+        print('explained variance ratio of test: {}'.format(pca_test.explained_variance_ratio_))
+        '''
         '''
         model = CNN_LSTM_model(image_dim=(frame_h,frame_w,channels),
                                audio_vector_dim=audio_vector_dim,
                                learning_rate=lr,
                                weight_init=ws)
         '''
+        
         model = create_model(image_dim=(frame_h,frame_w,channels),
                               audio_vector_dim=audio_vector_dim,
                               learning_rate=lr,
                               weight_init=ws,
                               output_dim=output_dim,
                               optimizer=optimizer)
-        # load custom callbacksカスタムコールバック!?あとでサーベイ
+        
+        '''
+        model = AlexNet_model(image_dim=(frame_h,frame_w,channels),
+                              audio_vector_dim=audio_vector_dim,
+                              learning_rate=lr,
+                              weight_init=ws,
+                              output_dim=output_dim,
+                              optimizer=optimizer)
+        '''
         #loss_history = LossHistory()
         #acc_history = AccuracyHistory()
         #callbacks_list = [loss_history, acc_history]
 
         # train the model #IMPUT space time image & RGB image , OUTPUT decomposed audio vector
-        fit=model.fit([dataX_train , dataZ_train],
-                  dataY_train_pca,
-                  shuffle='batch',
-                  epochs=10,
-                  batch_size=20,  # 10000 is the maximum number of samples that fits on TITANX 12GB Memory
+        fit=model.fit([dataX_train , dataZ_train],dataY_train_pca,epochs=50,batch_size=70,shuffle=False,verbose=1,validation_split=0.1)
                   #validation_data=([dataX_test,dataZ_test], dataY_test),
-                  verbose=1)
                   #callbacks = callbacks_list)
-        model.save('my_model.h5')
+        #fit=model.fit(dataX_train,dataY_train_pca,shuffle=False,epochs=50,batch_size=50,verbose=1,validation_split=0.1)
+        model.save('twostream_model.h5')
+        #fit=model.fit(dataZ_test,dataY_test_pca,shuffle=False,epochs=2,batch_size=10,verbose=1,validation_split=0.1)
+        #model.save('rgb_test_model.h5')
         #model = load_model('my_model.h5')
         #可視化
         # フォルダの作成
@@ -133,9 +141,9 @@ model_utils内の関数
             plt.xlabel('epoch')
             plt.ylabel('loss')
             plt.legend(loc='upper right')
-
+            plt.ylim(0, 50)
         plot_history_loss(fit)
-        plt.savefig(folder + '/vis_loss.pdf')
+        plt.savefig(folder + '/vis_twostream_loss.pdf')
         '''
         # Graph training history
         plotAndSaveData(loss_history, "Loss", learning_rate_hp=lr, weight_hp=ws, title="Loss History")
